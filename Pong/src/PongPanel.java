f9a30f0d3311df13f7c06b709c7464d29efa51cc
Josh Wallace <JoshWallaceNZ1@gmail.com>
2022-08-21 15:43:30 +1200

    First pong paddle drawn

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -4,14 +4,24 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	private final static Color BackgroundColor = Color.BLACK;
+	private final static int TimerDelay = 5;
 	
 	public PongPanel() {
 		setBackground(BackgroundColor);
+		Timer timer = new Timer(TimerDelay, this);
+        timer.start();
 	}
+	
+	 private void update() {
+         
+	 }
 
 	@Override
 	public void keyTyped(KeyEvent e) {		
@@ -32,5 +42,14 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	public void actionPerformed(ActionEvent e) {
 		
 	}
+	
+	 @Override
+	 public void paintComponent(Graphics g) {
+	     super.paintComponent(g);
+	     g.setColor(Color.WHITE);
+	     g.fillRect(20, 20, 30, 200);
+	 }
+	
+	
 
 }
